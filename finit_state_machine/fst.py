f80979f0ace5c4200ae37332b7e39327d0438c2d
ovednagar <ovednagar@gmail.com>
2019-03-21 22:59:10 +0200

    add -intersectio/union of fst lists, -FST supports multiple accepte states

--- finit_state_machine/fst.py
+++ finit_state_machine/fst.py
@@ -2,7 +2,7 @@ from random import randint, choice
 import networkx as nx
 EPS_MOVE = "EPS_MOVE"
 ACCEPT_SYMBOL = "ACC_SYM"
-ACCEPT_STATE = "ACC_STATE"
+ART_ACCEPT_STATE = "ART_ACC_STATE"
 REJECT_STATE = "REJ_STATE"
 
 
@@ -13,21 +13,23 @@ class State:
     init params
     state_name:     the states name ( which is the source )
     transition:     a dictionary of transitions { symbol: target_state, weight<int> }
+                    ** if a transition (state, symbol) is not defined then its assumed to be reject **
     is_init:        True if its an initial state
     is_accept:      True if its an accept state
     """
     def __init__(self, state_name, reject_state, transitions: dict= None, is_init=False, is_accept=False,
                  is_reject=False, artificial_accept=False):
-        self._source = state_name
-        self._reject_state = reject_state
-        self._transition = transitions if transitions else {}
+        self._source = state_name                               # states name
+        self._reject_state = reject_state                       # reject state used in case invalid symbol is used
         self._is_initial_state = is_init
         self._is_accept_state = is_accept
         self._is_reject_state = is_reject
-        self._is_art_accept_state = artificial_accept
+        self._is_art_accept_state = artificial_accept           # not real state from user input
+        self._transition = transitions if transitions else {}   # transition dict { symbol: (target, weight) }
         self._weights = [("sym", "<<>>", 0)]
-        self._edited = True
-        self._weighted = False
+        self._edited = True                                     # true if a transition is edited
+        self._base_weight = None                                # weight of last edited transition
+        self._weighted = False                                  # true if there is different weight for transitions
 
     @property
     def id(self):
@@ -57,13 +59,18 @@ class State:
     def transitions(self):
         return self._transition
 
+    def transition(self, symbol):
+        return self._transition[symbol] if symbol in self._transition else (self._reject_state, 0)
+
     """
     this function edits a transition according to input 
     """
     def edit_transition(self, symbol, target, weight=1):
         self._edited = True
         # picking by weights is rather expensive, thus a flag will be raise if there are any weights at all
-        if weight != 1:
+        if self._base_weight is None:
+            self._base_weight = weight
+        if weight != self._base_weight:
             self._weighted = True
         self._transition[symbol] = (target, weight)
 
@@ -143,20 +150,52 @@ class FST:
     alphabet:       list of alphabet symbols
     states:         set<preferred>/list/tuple of states<strings/chars/int>
     start_state:    a single initial state
-    accept_state:   a single accept state
+    accept_state:   list of accept states [ ...(qi, weight)...) or [ ...qi...] in that case weight are 1
     transitions:    list of tuples ( source_state, symbol, target_state, weight<int><optional>)
     """
-    def __init__(self, alphabet, states, start_state, accept_state, accept_weight, transitions: list):
-        self._alphabet = alphabet
-        self._states = states
-        self._start_state = start_state
-        self._accept_state = accept_state
-        self._transitions = self._build_transitions(states, start_state, accept_state, accept_weight, transitions)
+    def __init__(self, alphabet, states: set, start_state, accept_states: list, transitions: list):
+        self._alphabet = alphabet               # list [a, b, ...]
+        self._states = states                   # list [q0, q1, ...]
+        self._start_state = start_state         # q0
+        # self._accept_states = [ ... (qi, weight) ... ]
+        self._accept_states = [(s, 1) for s in accept_states] if type(accept_states[0]) is str else accept_states
+        # dictionary { q: State(q) } ... State has an attribute->transition which is a dict { symbol : (State, weight) }
+        # the dictionary includes the artificial ACCEPT_STATE { state_name: State object }
+        self._transitions = self._build_transitions(states, start_state, self._accept_states, transitions)
+
+    @property
+    def state_list(self):
+        return list(self._states)
+
+    @property
+    def state_names(self):
+        return self._states
+
+    @property
+    def alphabet(self):
+        return self._alphabet
+
+    @property
+    def start_state(self):
+        return self._start_state
+
+    def accept_states(self, weight=False):
+        for acc_state in self._accept_states:
+            yield acc_state[0] if weight else acc_state
+
+    def state(self, name):
+        return self._transitions[name]
+
+    def states(self):
+        for name, state in self._transitions.items():
+            if name == ART_ACCEPT_STATE:
+                continue
+            yield name, state
 
     def __str__(self):
         out_lines = []
-        max_len_state = max(len(max(self._states, key= lambda x: len(x))) + 2, 20)
-        max_len_aphabet = max(len(max(self._alphabet, key= lambda x: len(x))) + 2, 20)
+        max_len_state = max(len(max(self._states, key=lambda x: len(x))) + 2, 20)
+        max_len_alphabet = max(len(max(self._alphabet, key=lambda x: len(x))) + 2, 20)
 
         out_lines.append("Alphabet")
         for symbol in self._alphabet:
@@ -167,28 +206,28 @@ class FST:
                          + "     type")
 
         for state in sorted(self._states):
-            if state == ACCEPT_STATE:
+            if state == ART_ACCEPT_STATE:
                 continue
             out_lines.append(state + " " * int(max_len_state - len(state)) + "||"
                              + ("  -initial_state" if self._transitions[state].is_init else "")
-                             + ("  -accept_accept" if self._transitions[state].is_accept else "")
-                             + ("  -reject_reject" if self._transitions[state].is_reject else "")
+                             + ("  -accept_state" if self._transitions[state].is_accept else "")
+                             + ("  -reject_state" if self._transitions[state].is_reject else "")
                              )
 
         out_lines.append("\n\nTransitions")
         out_lines.append(" " * int((max_len_state - 6) / 2) + "Source" + " " * int((max_len_state - 6) / 2) + "||" +
-                         " " * int((max_len_aphabet - 6) / 2) + "Symbol" + " " * int((max_len_aphabet - 6) / 2) + "||" +
+                         " " * int((max_len_alphabet - 6) / 2) + "Symbol" + " " * int((max_len_alphabet - 6) / 2) + "||" +
                          " " * int((max_len_state - 6) / 2) + "Target" + " " * int((max_len_state - 6) / 2) + "||" +
                          "     Weight"
                          )
 
         for state in sorted(self._states):
-            if state == ACCEPT_STATE:
+            if state == ART_ACCEPT_STATE:
                 continue
             tran = self._transitions[state].transitions
             for symbol, (target, weight) in tran.items():
                 out_lines.append(state + " " * int(max_len_state - len(state)) + "||"
-                                 + " " + symbol + " " * int((max_len_aphabet - len(symbol) - 1)) + "||"
+                                 + " " + symbol + " " * int((max_len_alphabet - len(symbol) - 1)) + "||"
                                  + " " + target.id + " " * int((max_len_state - len(target.id) - 1)) + "||"
                                  + " " + str(weight)
                                  )
@@ -201,7 +240,7 @@ class FST:
      - a graph (gnx) is generated according to the transition 
      - check if path exists from node to accept_state for all nodes in V -> if there isn't then it's a reject_state
     """
-    def _get_reject_states(self, transitions, accept_state):
+    def _get_reject_states(self, transitions, accept_states):
         # build FST graph
         gnx = nx.DiGraph()
         list_edges = []
@@ -213,7 +252,12 @@ class FST:
         # check if there is a path from every node(state) to accept state if not add to reject_states
         reject_states = set()
         for node in gnx:
-            if not nx.has_path(gnx, node, accept_state):
+            reject_flag = True
+            for acc_state, acc_weight in accept_states:
+                # if there is a path to at least one accept node
+                if nx.has_path(gnx, node, acc_state):
+                    reject_flag = False
+            if reject_flag:
                 reject_states.add(node)
         return reject_states
 
@@ -222,15 +266,18 @@ class FST:
     { state_name: State_object }
     the state object includes all transitions and weight for a specific state - more info above at State class   
     """
-    def _build_transitions(self, states, init_state, accept_state, accept_weight, transitions):
-        reject_states = self._get_reject_states(transitions, accept_state)
+    def _build_transitions(self, states, init_state, accept_states, transitions):
+        accept_set = set([s for s, w in accept_states])
+        reject_states = self._get_reject_states(transitions, accept_states)
         global_reject_state = State(REJECT_STATE, None, is_reject=True)  # artificial reject state
+        self._states.add(REJECT_STATE)
         # build a dictionary of name to State objects
-        state_dict = {q: State(q, global_reject_state, is_init=q == init_state, is_accept=q == accept_state,
+        state_dict = {q: State(q, global_reject_state, is_init=q == init_state, is_accept=q in accept_set,
                                is_reject=q in reject_states) for q in states}
         # add a artificial node for accept state
-        state_dict[ACCEPT_STATE] = State(ACCEPT_STATE, global_reject_state, artificial_accept=True)
-        transitions.append((accept_state, ACCEPT_SYMBOL, ACCEPT_STATE, accept_weight))
+        state_dict[ART_ACCEPT_STATE] = State(ART_ACCEPT_STATE, global_reject_state, artificial_accept=True)
+        for acc_state, acc_weight in accept_states:
+            transitions.append((acc_state, ACCEPT_SYMBOL, ART_ACCEPT_STATE, acc_weight))
         for tran in transitions:
             source, symbol, target, weight = tran if len(tran) == 4 else list(tran) + [1]
             state_dict[source].edit_transition(symbol, state_dict[target], weight=weight)
@@ -281,7 +328,7 @@ test: the following FST represents the language L={ a^n b^n || n,m > 0 }
 -----------------------
   q0  |  q1   |  q3   |
   q1  |  q1   |  q2   |
-  q2  |  q3   |  q2   |
+  q2  |  q3   |  q2   | acc=3
   q3  |  q3   |  q3   |
   
   
@@ -300,8 +347,7 @@ if __name__ == "__main__":
     _alphabet = ["a", "b"]
     _states = {"q0", "q1", "q2", "q3"}
     _init_state = "q0"
-    _accept_state = "q2"
-    _accept_weight = 1
+    _accept_states = [("q2", 3)]
     _transitions = [
         ("q0", "a", "q1"),
         ("q0", "b", "q3"),
@@ -312,7 +358,7 @@ if __name__ == "__main__":
         ("q3", "a", "q3"),
         ("q3", "b", "q3")
     ]
-    _fst = FST(_alphabet, _states, _init_state, _accept_state, _accept_weight, _transitions)
+    _fst = FST(_alphabet, _states, _init_state, _accept_states, _transitions)
     print("unweighted FST")
     print(_fst)
     assert _fst.go("aaabbbbb")[1]
@@ -325,8 +371,7 @@ if __name__ == "__main__":
     _alphabet = ["a", "b"]
     _states = {"q0", "q1", "q2", "q3"}
     _init_state = "q0"
-    _accept_state = "q2"
-    _accept_weight = 3
+    _accept_states = [("q2", 3)]
     _transitions = [
         ("q0", "a", "q1"),
         ("q0", "b", "q3"),
@@ -337,7 +382,7 @@ if __name__ == "__main__":
         ("q3", "a", "q3"),
         ("q3", "b", "q3")
     ]
-    _fst = FST(_alphabet, _states, _init_state, _accept_state, _accept_weight, _transitions)
+    _fst = FST(_alphabet, _states, _init_state, _accept_states, _transitions)
     print("\n\nweighted FST")
     print(_fst)
     assert _fst.go("aaabbbbb")[1]
